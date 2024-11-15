e9aaf9e10c146fe46257cce1f3bd160f6842a262
jess <v.jessica@escolar.ifrn.edu.br>
2024-11-15 17:37:45 -0300

    atualizando cores e corrigindo bugs

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// alteração: função para mostrar o conteúdo dinâmico
-function showContent(index) {
-    const contentDiv = document.getElementById("content");
-    contentDiv.innerHTML = `<p>${contents[index]}</p>`;
-    
-// alteração: reaplica o ScrollReveal após a atualização do conteúdo
-    ScrollReveal().reveal('.content', { delay: 100, reset: true });
-};
-
-// alteração: função para executar o ScrollReveal após o carregamento completo da página
-window.onload = function() {
-    ScrollReveal().reveal('.hero', { delay: 100 });
-    ScrollReveal().reveal('.sobre', { delay: 200 });
-    ScrollReveal().reveal('.noticias', { delay: 300 });
-    ScrollReveal().reveal('.card-cartilha', { delay: 400 });
-    ScrollReveal().reveal('.content', { delay: 100, reset: true });
-};
-
-
 $(document).ready(function(){
     $('.news-carousel').slick({
         infinite: true,
